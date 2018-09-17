0b3a168b0f8330a8a8b7702a2eec453b9be94391
mauronofrio matarrese <poronofrio@gmail.com>
2018-09-17 13:00:24 +0200

    Adding Pixel Experience stuff

--- aosp_nx551j.mk
+++ aosp_nx551j.mk
@@ -15,8 +15,13 @@
 #
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1080
-TARGET_SCREEN_WIDTH := 1920
+#TARGET_SCREEN_HEIGHT := 1080
+#TARGET_SCREEN_WIDTH := 1920
+
+
+# PixelExperience stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
 
 # Inherit 64-bit configs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
