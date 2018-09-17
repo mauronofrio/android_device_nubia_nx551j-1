d486fde2f89ed6878ea8e0b1271e88ae362388a1
mauronofrio matarrese <poronofrio@gmail.com>
2018-09-17 11:32:33 +0200

    nx551j: Pixel Experience

--- aosp_nx551j.mk
+++ aosp_nx551j.mk
@@ -27,11 +27,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_ARCH := arm64
+TARGET_DENSITY := xxhdpi
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Inherit device configuration
 $(call inherit-product, device/nubia/nx551j/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_nx551j
+PRODUCT_NAME := aosp_nx551j
 PRODUCT_DEVICE := nx551j
 PRODUCT_BRAND := nubia
 PRODUCT_MODEL := Nubia M2
